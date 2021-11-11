a28dcd04e3eadb02673857395f159ea54d18a541
DESKTOP-IEE2V5I\admin <mvoznyuk@partners.org>
2021-11-11 05:34:38 +0200

    Added basic messaging to user when downloaded started

--- app/main.py
+++ app/main.py
@@ -35,10 +35,20 @@ def download_utube(update, context: CallbackContext):
     url = update.message.text
     if is_supported(url):
         with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-            info = ydl.extract_info(url, download=True)
-            filename = ydl.prepare_filename(info).rsplit('.', 1)[0]+".mp3"
+            info = ydl.extract_info(url, download=False)
+            raw_filename = ydl.prepare_filename(info)
+            filename = raw_filename.rsplit('.', 1)[0]+".mp3"
+        message = context.bot.send_message(
+            chat_id=update.effective_chat.id,
+            text=f"Download Started: {filename}"
+        )
+        info = ydl.extract_info(url, download=True)
+        context.bot.edit_message_text(
+            chat_id=update.effective_chat.id, message_id=message.message_id, text=f"Convert Started:: {filename}")
         context.bot.send_audio(
             chat_id=update.effective_chat.id, audio=open(filename, 'rb'))
+        context.bot.delete_message(
+            chat_id=update.message.chat.id, message_id=message.message_id)
         context.bot.delete_message(
             chat_id=update.message.chat.id, message_id=update.message.message_id)
     try:
